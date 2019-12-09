42a955e6e84a87263fcb86772f13f51e2f8e531c
Night Fury <banyerhan@gmail.com>
2019-12-09 09:29:37 +0630

    Update functions.php

--- functions.php
+++ functions.php
@@ -56,4 +56,44 @@ return $fields;
 }
 add_filter('woocommerce_checkout_fields','custom_override_checkout_fields');
 
+// Hide add to cart for ecommerce
+remove_action('woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart');
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 20);
+add_action( 'woocommerce_single_product_summary', 'remove_simple_product_add_to_cart_button', 1 );
+function remove_simple_product_add_to_cart_button() {
+    global $product;
+
+    // For simple products type
+    if( $product->is_type( 'simple' ) && $product->is_purchasable() ) {
+        remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+        add_action( 'woocommerce_single_product_summary', 'show_stock_info', 30 );
+    }
+}
+add_filter( 'woocommerce_is_purchasable', '__return_false' );
+// Function that stock info
+function show_stock_info() {
+    global $product;
+
+    echo wc_get_stock_html( $product );
+}
+
+// Hide Price function
+add_filter( 'woocommerce_get_price_html', 'react2wp_woocommerce_hide_product_price' );
+function react2wp_woocommerce_hide_product_price( $price ) {
+    return '';
+}
+
+function woo_related_products_limit() {
+  global $product;
+	
+	$args['posts_per_page'] = 6;
+	return $args;
+}
+add_filter( 'woocommerce_output_related_products_args', 'nf_related_products_args', 20 );
+  function nf_related_products_args( $args ) {
+	$args['posts_per_page'] = 6; // 6 related products
+	$args['columns'] = 6; // arranged in 5 columns
+	return $args;
+}
+
 ?>
