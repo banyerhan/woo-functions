a625c444c1e48a1f8a933fc5de9fb68bda2b7eef
NightFury <banyerhan@gmail.com>
2021-03-09 13:19:56 +0630

    restricted price
    hide price for visitors

--- functions.php
+++ functions.php
@@ -128,4 +128,21 @@ function custom_cart_button_text( $html ) {
 endif;
 }
 
+//Resticed price only register users only...
+function hide_price_add_cart_not_logged_in() {   
+   if ( ! is_user_logged_in() ) {      
+      remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+      remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+      remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10 );
+      remove_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 ); 
+      remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart');
+      add_action( 'woocommerce_single_product_summary', 'print_login_to_see', 31 );
+      add_action( 'woocommerce_after_shop_loop_item', 'print_login_to_see', 11 );
+      add_filter( 'woocommerce_add_to_cart_redirect', 'my_redirect' ); 
+   }
+}
+function print_login_to_see() {
+   echo '<a href="' . get_permalink(wc_get_page_id('myaccount')) . '">' . __('Only registered to see prices', 'peopleorigin') . '</a>';
+}
+
 ?>
